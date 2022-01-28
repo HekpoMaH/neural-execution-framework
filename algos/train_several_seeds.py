4f9454d6a46202cbddeda5d9a150801b3f2dff2b
HekpoMaH <dobrikg666@gmail.com>
2022-01-28 15:31:25 +0200

    skim concepts on this one

--- algos/train_several_seeds.py
+++ algos/train_several_seeds.py
@@ -13,11 +13,6 @@ Options:
 
     --use-GRU              Force the usage of a GRU cell at the GNN update step.
 
-    --no-use-concepts      Do NOT utilise concepts bottleneck. If you set this
-                           flag, set below flag too. [default: False]
-
-    --no-use-concepts-sv   Do NOT utilise concepts supervision. [default: False]
-
     --pooling=PL           What graph pooling mechanism to use for termination.
                            One of {attention, max, mean, predinet}. [default: predinet]
 
@@ -37,19 +32,11 @@ Options:
 
     --starting-seed SS     Which seed index to start from [default: 0]
 
-    --L1-loss              Add L1 loss to the concept decoders part. [default: False]
-
-    --prune-epoch PE       The epoch on which to prune logic (currently termination only!)
-                           layers. The default of -1 does no pruning at all. [default: -1]
-
-    --drop-last-concept    Drop last concept? (Works only for coloring) [default: False]
-
-    --use-decision-tree    Use decision tree for concept->output mapping. [default: False]
-
     --CUDA-START CS        From which cuda device to start [default: 0]
 
     --CUDA-MOD CM          How many machines to cycle [default: 1]
 
+    --dry                  Dry run. Just print commands, nothing else. [default: False]
 """
 import os
 import schema
@@ -59,20 +46,15 @@ args = docopt(__doc__)
 schema = schema.Schema({'--algos': schema.And(list, [lambda n: n in ['BFS', 'parallel_coloring']]),
                         '--help': bool,
                         '--use-TF': bool,
-                        '--no-use-concepts-sv': bool,
-                        '--no-use-concepts': bool,
                         '--pooling': schema.And(str, lambda s: s in ['attention', 'mean', 'max', 'predinet']),
                         '--no-next-step-pool': bool,
                         '--use-GRU': bool,
-                        '--use-decision-tree': bool,
                         '--no-patience': bool,
-                        '--L1-loss': bool,
-                        '--drop-last-concept': bool,
+                        '--dry': bool,
                         '--num-seeds': schema.Use(int),
                         '--starting-seed': schema.Use(int),
                         '--CUDA-MOD': schema.Use(int),
                         '--CUDA-START': schema.Use(int),
-                        '--prune-epoch': schema.Use(int),
                         '--model-name': schema.Or(None, schema.Use(str)),
                         '--epochs': schema.Use(int)})
 args = schema.validate(args)
@@ -80,22 +62,24 @@ args = schema.validate(args)
 commands = []
 for seed in range(args['--starting-seed'], args['--starting-seed']+args['--num-seeds']):
     machine = args['--CUDA-START'] + seed % args['--CUDA-MOD']
-    command = f'CUDA_VISIBLE_DEVICES={machine} python -m algos.train --epochs {args["--epochs"]} --model-name {args["--model-name"]}_seed_{seed} --prune-epoch {args["--prune-epoch"]} --pooling {args["--pooling"]} '
+    command = f'CUDA_VISIBLE_DEVICES={machine} python -m algos.train --epochs {args["--epochs"]} --model-name {args["--model-name"]}_seed_{seed} --pooling {args["--pooling"]} '
 
     for algo in args['--algos']:
         command += f'--algos {algo} '
 
     for flag in [
-            '--use-TF', '--use-GRU', '--no-patience', '--no-use-concepts',
-            '--no-use-concepts-sv', '--L1-loss', '--drop-last-concept',
-            '--no-next-step-pool', '--use-decision-tree'
+            '--use-TF', '--use-GRU', '--no-patience',
+            '--no-next-step-pool'
     ]:
         if args[flag]:
             command += flag + ' '
     command += f'--seed {seed} '
     command += '&'
     commands.append(command)
+
 print(commands)
+if args['--dry']:
+    exit(0)
 
 with open("runseeds.sh", 'w+') as f:
     for command in commands:
